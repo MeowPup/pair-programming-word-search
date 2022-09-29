94c508148eb498e3fdc2ba34d8df2e84d7d59734
EVillett <emily.villett@gmail.com>
2022-09-28 18:30:07 -0700

    submitting completed wordsearch file

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  
+  if (letters === []) {
+    return false;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word))
+      return true;
+  }
+
+  for (let row = 0; row < horizontalJoin.length; row++) {
+    let array = [];
+    for (let col = 0; col < horizontalJoin[row].length; col++) {
+      array.push(horizontalJoin[col][row]);
+    }
+    let newArray = array.join('');
+    if (newArray.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+module.exports = wordSearch;
